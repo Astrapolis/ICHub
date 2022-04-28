2ec28e97d1db67bdc9d79dfd63378f07bd024a1c
crypto_jedi <ybn1992@gmail;.com>
2022-04-28 23:48:27 +0800

    1 config per canister + canister metadata

--- src/ICHub/src/lib.rs
+++ src/ICHub/src/lib.rs
@@ -1,60 +1,77 @@
-use candid::{Deserialize, Principal, CandidType, candid_method};
+use candid::{Deserialize, Principal, CandidType, candid_method, IDLProg, TypeEnv, check_prog, idl_hash};
 use serde::Serialize;
 use std::cell::RefCell;
-use std::collections::{VecDeque, HashMap};
+use std::collections::{VecDeque};
 use std::string::String;
 use ic_cdk_macros;
-use ic_cdk::{api};
+use ic_cdk::api;
 
 thread_local! {
-    pub static USER_CONFIGS: std::cell::RefCell<HashMap<Principal, UserConfig>>  = 
-    RefCell::new(HashMap::new());
+    pub static USER_CONFIGS: std::cell::RefCell<UserConfig>  = 
+    RefCell::new(UserConfig::new());
 }
 
 #[derive(CandidType, Deserialize, Serialize, Debug, Clone)]
 pub struct CanisterCall {
     canister_id: Principal,
     function_name: String,
+    time_at: u64,
     params: Vec<u8>,
     result: Vec<u8>,
 }
 
 #[derive(CandidType, Debug, Deserialize, Clone, Serialize)]
+pub struct CanisterMeta {
+    moudule_hash : u32,
+    controller: Principal,
+    time_updated: u32,
+    did_file: String
+}
+
+#[derive(CandidType, Debug, Deserialize, Clone, Serialize)]
 pub struct CanisterConfig {
-    time_updated: u64,
     canister_id: Principal,
+    time_updated: u64,
     is_active: bool,
-    config: Vec<u8>
+    is_local: bool,
+    is_public: bool,
+    config: Vec<u8>,
+    meta_data: Vec<CanisterMeta>
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct UserConfig {
     user: Principal,
-    calls_limit: u16,
+    calls_limit: u32,
     ui_config: Vec<u8>,
     canister_configs: Vec<CanisterConfig>,
     canister_calls: VecDeque<CanisterCall>,
 }
 
 #[derive(CandidType)]
-pub struct UserConfigDisplay {
+pub struct UserConfigView {
     user:  Principal,
-    calls_limit: u16,
     ui_config: Vec<u8>,
     canister_configs: Vec<CanisterConfig>,
-    canister_calls: Vec <CanisterCall>
+    canister_calls: Vec<CanisterCall>
 }
 
-
 impl UserConfig {
-    fn init(user: Principal, calls_limit: u16, ui_config : &Vec<u8>, canister_configs : &Vec<CanisterConfig>) -> Self {
+    fn new() -> Self{
         UserConfig {
-            user,
-            calls_limit,
-            ui_config : ui_config.clone(),
-            canister_configs: canister_configs.clone(),
+            user : Principal::anonymous(),
+            ui_config : Vec::new(),
+            calls_limit: u32::MAX,
+            canister_configs: Vec::new(),
             canister_calls: VecDeque::new()
         }
+    }
+
+    fn init(&mut self, user: Principal, calls_limit: u32, ui_config : &Vec<u8>,  canister_configs : &Vec<CanisterConfig>) {
+        self.user = user;
+        self.calls_limit = calls_limit;
+        self.ui_config = ui_config.clone();
+        self.canister_configs = canister_configs.clone();
     }
 
     fn update_ui_config(&mut self, ui_config : &Vec<u8>) {
@@ -62,30 +79,19 @@ impl UserConfig {
     }
 
     fn update_canister_config(&mut self, canister_config : &CanisterConfig){
-        let num_configs  = self.canister_configs.len();
-        if num_configs == 0 {
-            self.canister_configs.push(canister_config.clone());
-        }
-        else {
-            let mut oldest_config = &mut self.canister_configs[0].clone(); 
-            for  config in &mut self.canister_configs {
-                if config.canister_id == canister_config.canister_id {
-                    *config = canister_config.clone();
-                    return ;
-                }
-                oldest_config = if oldest_config.time_updated < config.time_updated && config.is_active == false
-                {oldest_config} else {config};
-            }
-            match num_configs as u8 == u8::MAX {
-                true => {*oldest_config = canister_config.clone()}
-                false => {self.canister_configs.push(canister_config.clone())}
+        for  config in &mut self.canister_configs {
+            if config.canister_id == canister_config.canister_id 
+                && config.is_local == canister_config.is_local{
+                *config = canister_config.clone();
+                return ;
             }
         }
+        self.canister_configs.push(canister_config.clone());
     }
 
     fn insert_canister_calls(&mut self, canister_calls : &Vec<CanisterCall>){
         for canister_call in canister_calls {
-            match self.canister_calls.len() as u16 == self.calls_limit {
+            match self.canister_calls.len() as u32 == self.calls_limit {
                 true => {
                     self.canister_calls.pop_back();
                     self.canister_calls.push_front(canister_call.clone())
@@ -95,15 +101,25 @@ impl UserConfig {
         }
     }    
 
-    fn get_user_config(&self) -> UserConfigDisplay {
-        UserConfigDisplay{
+    fn get_user_config(&self) -> UserConfigView {
+        UserConfigView{
             user: self.user,
-            calls_limit: self.calls_limit,
             ui_config: self.ui_config.clone(),
-            canister_configs: self.canister_configs.clone(),
+            canister_configs: self.get_canisters_configs(true, false),
             canister_calls: self.get_canister_calls(None, None, Some(100))
         }
     }
+
+    fn get_canisters_configs(&self, is_active: bool, is_public: bool) -> Vec<CanisterConfig>
+    {
+        if is_public {
+            self.canister_configs.iter().filter(|config| 
+                config.is_active == is_active && config.is_public == is_public).cloned().collect()
+        } else {
+            self.canister_configs.iter().filter(|config| 
+                config.is_active == is_active).cloned().collect()            
+        }
+    }     
 
     fn get_canister_calls(&self, canister_id: Option<Principal>, function_name: Option<String>, limit: Option<u16>) -> Vec<CanisterCall>
     {
@@ -124,11 +140,11 @@ impl UserConfig {
 
 #[ic_cdk_macros::update(name = "user_init")]
 #[candid_method(update, rename = "user_init")]
-async fn user_init(ui_config : Vec<u8>, canister_configs : Vec<CanisterConfig>){
+async fn user_init(calls_limit: u32, ui_config : Vec<u8>, canister_configs : Vec<CanisterConfig>){
     USER_CONFIGS.with(|config| {
-        let mut configs = config.borrow_mut();
+        let mut config = config.borrow_mut();
         let caller = api::caller();
-        configs.entry(caller).or_insert(UserConfig::init(caller, 100, &ui_config, &canister_configs));
+        config.init(caller, calls_limit, &ui_config, &canister_configs);
     }        
     );
 }
@@ -138,8 +154,7 @@ async fn user_init(ui_config : Vec<u8>, canister_configs : Vec<CanisterConfig>){
 async fn cache_ui_config(ui_config : Vec<u8>){
     USER_CONFIGS.with(|config| {
         let mut config = config.borrow_mut();
-        let user_config = config.get_mut(&api::caller()).unwrap();
-        user_config.update_ui_config(&ui_config);
+        config.update_ui_config(&ui_config);
     }        
     );
 }
@@ -149,8 +164,7 @@ async fn cache_ui_config(ui_config : Vec<u8>){
 async fn cache_canister_config(canister_config : CanisterConfig){
     USER_CONFIGS.with(|config| {
         let mut config = config.borrow_mut();
-        let user_config = config.get_mut(&api::caller()).unwrap();
-        user_config.update_canister_config(&canister_config);
+        config.update_canister_config(&canister_config);
     }        
     );
 }
@@ -160,19 +174,17 @@ async fn cache_canister_config(canister_config : CanisterConfig){
 async fn cache_canister_calls(canister_calls : Vec<CanisterCall>){
     USER_CONFIGS.with(|config| {
         let mut config = config.borrow_mut();
-        let user_config = config.get_mut(&api::caller()).unwrap();
-        user_config.insert_canister_calls(&canister_calls);
+        config.insert_canister_calls(&canister_calls);
     }        
     );
 }
 
 #[ic_cdk_macros::query(name = "get_user_config")]
 #[candid_method(query, rename = "get_user_config")]
-fn get_user_config() -> UserConfigDisplay{
+fn get_user_config() -> UserConfigView{
     USER_CONFIGS.with(|config| {
         let config = config.borrow();
-        let user_config = config.get(&api::caller()).unwrap();
-        user_config.get_user_config()
+        config.get_user_config()
     }        
     )
 }
@@ -188,9 +200,27 @@ fn get_candid_principal() -> Principal{
 fn get_canister_calls(canister_id: Option<Principal>, function_name: Option<String>, limit: Option<u16>) -> Vec<CanisterCall>{
     USER_CONFIGS.with(|config| {
         let config = config.borrow();
-        let user_config = config.get(&api::caller()).unwrap();
-        user_config.get_canister_calls(canister_id, function_name, limit)
+        config.get_canister_calls(canister_id, function_name, limit)
     })   
+}
+
+#[ic_cdk_macros::query(name = "get_public_configs")]
+#[candid_method(query, rename = "get_public_configs")]
+fn get_public_configs() -> Vec<CanisterConfig>{
+    USER_CONFIGS.with(|config| {
+        let config = config.borrow();
+        config.get_canisters_configs(true, true)
+    })   
+}
+
+#[ic_cdk_macros::query(name = "did_to_js")]
+#[candid_method(query, rename = "did_to_js")]
+fn did_to_js(prog: String) -> Option<String> {
+    let ast = prog.parse::<IDLProg>().ok()?;
+    let mut env = TypeEnv::new();
+    let actor = check_prog(&mut env, &ast).ok()?;
+    let res = ic_cdk::export::candid::bindings::javascript::compile(&env, &actor);
+    Some(res)
 }
 
 candid::export_service!();
