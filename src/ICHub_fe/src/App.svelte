9697e781bb78558090d6f3f74694d6a9e3419008
liming <liming32@baidu.com>
2022-04-29 08:40:53 +0800

    feat:增加获取指定canister的ActorClass的功能，并增加ui界面元素

--- src/ICHub_fe/src/App.svelte
+++ src/ICHub_fe/src/App.svelte
@@ -1,19 +1,46 @@
 <script>
-  // import CanisterIds from "./components/CanisterIds.svelte";
-  // import Links from "./components/Links.svelte";
+  import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
   import NewFollowCard from "./components/NewFollowCard.svelte";
+  import IconButton from "@smui/icon-button";
   import { Principal } from "@dfinity/principal";
   import { onMount } from "svelte";
 
   let login = true;
-  // let canisterId = "";
+  function onNewCanisterFollowed(event) {
+    console.log("onNewCanisterFollowed =>", event.detail);
+  }
 </script>
 
 <main>
+  <div class="top-app-bar-container flexor">
+    <TopAppBar variant="static" color="primary">
+      <Row>
+        <Section align="start">
+          <IconButton class="material-icons">menu</IconButton>
+          <Title>ICHub</Title>
+        </Section>
+      </Row>
+    </TopAppBar>
+  </div>
   {#if login}
-    <NewFollowCard />
+    <NewFollowCard on:newCanisterFollowed={onNewCanisterFollowed} />
   {/if}
 </main>
 
 <style>
+  .top-app-bar-container {
+    width: 100%;
+    border: 1px solid
+      var(--mdc-theme-text-hint-on-background, rgba(0, 0, 0, 0.1));
+    margin: 0 18px 18px 0;
+    background-color: var(--mdc-theme-background, #fff);
+
+    overflow: auto;
+    display: inline-block;
+  }
+
+  .flexor {
+    display: inline-flex;
+    flex-direction: column;
+  }
 </style>
--- src/ICHub_fe/src/components/NewFollowCard.svelte
+++ src/ICHub_fe/src/components/NewFollowCard.svelte
@@ -1,12 +1,53 @@
 <script>
-  import { IDL } from "@dfinity/candid";
+  import { createEventDispatcher } from "svelte";
+  import { Actor } from "@dfinity/agent";
+  import { Principal } from "@dfinity/principal";
+  import {
+    getProfilerActor,
+    getActorFromCanisterId,
+  } from "../utils/actorUtils";
   import Paper, { Title, Content } from "@smui/paper";
   import Textfield from "@smui/textfield";
   import Button, { Label } from "@smui/button";
+  import CircularProgress from "@smui/circular-progress";
+  import Snackbar, { Actions, Label as SLabel } from "@smui/snackbar";
+
+  const dispatch = createEventDispatcher();
   let newCanisterId = null;
-  function handleSubmitNewCanister(event) {
+  let formRef = null;
+  let following = false;
+  let snackContent = "";
+  let snackbarRef = null;
+  async function handleSubmitNewCanister(event) {
     event.preventDefault();
     console.log("submit with new canister id", newCanisterId);
+    following = true;
+    snackContent = "";
+    try {
+      const actor = await getActorFromCanisterId(
+        Principal.fromText(newCanisterId)
+      );
+
+      if (!!actor) {
+        dispatch("newCanisterFollowed", { actor, canisterId: newCanisterId });
+        formRef.reset();
+        snackContent = "新canister已成功加入观察列表。";
+      } else {
+        snackContent = "无法找到指定的canister。";
+      }
+      //   const sortedMethods = Actor.interfaceOf(actor)._fields.sort(([a], [b]) =>
+      //     a > b ? 1 : -1
+      //   );
+      //   for (const [name, func] of sortedMethods) {
+      //     // renderMethod(canister, name, func, profiler);
+      //     console.log("sortedMethods ===>", name, func);
+      //   }
+    } catch (err) {
+      console.log("get canister profile error", err);
+      snackContent = "出错啦，无法观察指定的canister。" + err;
+    }
+    following = false;
+    snackbarRef.open();
   }
 </script>
 
@@ -14,7 +55,10 @@
   <Paper>
     <Title>观察新的canister</Title>
     <Content>
-      <form on:submit={handleSubmitNewCanister}>
+      <Snackbar bind:this={snackbarRef}>
+        <SLabel>{snackContent}</SLabel>
+      </Snackbar>
+      <form on:submit={handleSubmitNewCanister} bind:this={formRef}>
         <div>
           <Textfield
             variant="outlined"
@@ -22,9 +66,16 @@
             label="请输入新的canister id"
             required
           />
-          <Button variant="raised" type="submit">
-            <Label>添加到观察列表</Label>
-          </Button>
+          {#if following}
+            <CircularProgress
+              style="height: 32px; width: 32px;"
+              indeterminate
+            />
+          {:else}
+            <Button variant="raised" type="submit">
+              <Label>添加到观察列表</Label>
+            </Button>
+          {/if}
         </div>
       </form>
     </Content>
