011fd6f7e9608045c1feb67f322f27cdfd3e7850
crypto_jedi <ybn1992@gmail;.com>
2022-05-31 16:29:33 +0800

    more tests

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ dfx canister call renrk-eyaaa-aaaaa-aaada-cai get_user_configs_by_user --query
 dfx canister call renrk-eyaaa-aaaaa-aaada-cai get_public_canister_states --query
 dfx canister call qaa6y-5yaaa-aaaaa-aaafa-cai get_user_config '(0 : nat16)' --query 
 dfx canister call qaa6y-5yaaa-aaaaa-aaafa-cai add_user_config '("adfadsfdasdfasfdasfdasf dasfasdfasf ads fasdfasdf asdf asdfasd fasdf dasf")'
+dfx canister call renrk-eyaaa-aaaaa-aaada-cai get_user_configs_by_user --query
 dfx canister call qaa6y-5yaaa-aaaaa-aaafa-cai cache_canister_calls '(0 : nat16, vec {record {canister_id = principal "renrk-eyaaa-aaaaa-aaada-cai"; function_name = "1234"; event = null}})' 
 dfx canister call qaa6y-5yaaa-aaaaa-aaafa-cai get_canister_calls '(0 : nat16)' --query
 dfx canister call qaa6y-5yaaa-aaaaa-aaafa-cai cache_test_case '(0 : nat16, record {test_case_id = null; tag="asdf"; config="asdf"; time_at = 12312312 : nat64; canister_calls = vec {record {canister_id = principal "renrk-eyaaa-aaaaa-aaada-cai"; function_name = "1234"; event = null}}})' 
