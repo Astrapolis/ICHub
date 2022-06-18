088fc4c3afb7cfc5d4eb47a097011eadec4a9f02
crypto_jedi <ybn1992@gmail;.com>
2022-06-18 12:13:38 +0800

    cycles

--- install_prod.sh
+++ install_prod.sh
@@ -2,5 +2,6 @@ dfx canister create devhub
 dfx build devhub
 dfx generate devhub
 
-dfx deploy hub_assets
-dfx canister call 4xlse-oyaaa-aaaal-qa6zq-cai register_new_canister '(10_639_159_999_990, 1000, "{version: 1};", true, 1000)'
+dfx deploy --network=ic hub --with-cycles 170558782354
+dfx deploy --network=ic hub_assets --with-cycles 170558782354
+dfx canister call 4xlse-oyaaa-aaaal-qa6zq-cai register_new_canister '(139_159_999_990, 1000, "{version: 1};", true, 1000)'
